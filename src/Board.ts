a0d99212e56b85b3da5e8dd0bd599528bc10294a
limunosekai <powerlsh0103@gmail.com>
2021-05-21 12:24:47 +0900

    Add Event

--- src/Board.ts
+++ src/Board.ts
@@ -44,6 +44,7 @@ export class Cell {
 export class Board {
   cells: Cell[] = [];
   _el: HTMLElement = document.createElement('div');
+  map: WeakMap<HTMLElement, Cell> = new WeakMap();
 
   constructor(upperPlayer: Player, lowerPlayer: Player) {
     this._el.className = 'board';
@@ -61,6 +62,7 @@ export class Board {
         });
 
         const cell = new Cell({ row, col }, piece);
+        this.map.set(cell._el, cell);
         this.cells.push(cell);
         rowEl.appendChild(cell._el);
       }      
--- src/Game.ts
+++ src/Game.ts
@@ -1,10 +1,15 @@
-import { Board, DeadZone } from './Board';
+import { Board, Cell, DeadZone } from './Board';
 import { Player, PlayerType } from './Player';
 import './Piece';
+import { Lion } from './Piece';
 
 export class Game {
+  private selectedCell: Cell;
   private turn = 0;
-  
+  private currentPlayer: Player;
+  private gameInfoEl = document.querySelector('.alert');
+  private state: 'STARTED' | 'END' = 'STARTED';
+
   readonly upperPlayer = new Player(PlayerType.UPPER);
   readonly lowerPlayer = new Player(PlayerType.LOWER);
   
@@ -17,6 +22,93 @@ export class Game {
     boardContainer.firstChild.remove();
     boardContainer.appendChild(this.board._el);
 
+    this.currentPlayer = this.upperPlayer;
+    
+    this.board.render();
+    this.renderInfo();
+
+    this.board._el.addEventListener('click', e => {
+      if(this.state === 'END') {
+        return false;
+      }
+
+      if(e.target instanceof HTMLElement) {
+        let cellEl: HTMLElement;
+        if(e.target.classList.contains('cell')) {
+          cellEl = e.target;
+        } else if (e.target.classList.contains('piece')) {
+          cellEl = e.target.parentElement;
+        } else {
+          return false;
+        }
+
+        const cell = this.board.map.get(cellEl);
+
+        if(this.isCurrentUserPiece(cell)) {
+          this.select(cell);
+          return false;
+        }
+
+        if(this.selectedCell) {
+          this.move(cell);
+          this.changeTurn();
+        }
+      }
+    })
+  }
+
+  isCurrentUserPiece(cell: Cell) {
+    return cell != null && cell.getPiece() != null && cell.getPiece().ownerType === this.currentPlayer.type;
+  }
+
+  select(cell: Cell) {
+    if(cell.getPiece() == null) { return }
+    
+    if(cell.getPiece().ownerType !== this.currentPlayer.type) { return }
+    
+    if(this.selectedCell) {
+      this.selectedCell.deActive();
+      this.selectedCell.render();
+    }
+
+    this.selectedCell = cell;
+    cell.active();
+    cell.render();
+  }
+
+  move(cell: Cell) {
+    this.selectedCell.deActive();
+    const killed = this.selectedCell.getPiece().move(this.selectedCell, cell).getKilled();
+    this.selectedCell = cell;
+
+    if(killed) {
+      if(killed.ownerType === PlayerType.UPPER) {
+        this.lowerDeadZonde.put(killed);
+      } else {
+        this.upperDeadZonde.put(killed);
+      }
+
+      if(killed instanceof Lion) {
+        this.state = 'END'
+      }
+    }
+  }
+
+  renderInfo(extraMessasge?: string) {
+    this.gameInfoEl.innerHTML = `#${this.turn}턴 ${this.currentPlayer.type} 차례 ${(extraMessasge) ? '| ' + extraMessasge : ''}`;
+  }
+  
+  changeTurn() {
+    this.selectedCell.deActive();
+    this.selectedCell = null;
+
+    if(this.state === 'END') {
+      this.renderInfo('END');
+    } else {
+      this.turn += 1;
+      this.currentPlayer = (this.currentPlayer === this.lowerPlayer) ? this.upperPlayer : this.lowerPlayer;
+      this.renderInfo();
+    }
     this.board.render();
   }
 }
